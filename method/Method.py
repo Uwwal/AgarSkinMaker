575636ef77df4bf9360023f8a81cef996aa6bf8b
Uwwal <69232066+Uwwal@users.noreply.github.com>
2021-05-31 23:49:36 +0800

    Update Method.py

--- method/Method.py
+++ method/Method.py
@@ -2,5 +2,5 @@ class Method:
     def __init__(self, language):
         self.language = language
 
-    def get_method_detail_weight(self):
+    def get_method_detail_weight(self, frame):
         pass
